ad506a90120443ba2145604c2d1c6a6962788bdf
BeastImran <imsalmanran789@gmail.com>
2024-08-12 16:03:36 +0530

    implement async fetch. Also a bug fix

--- brutalist_report/main.py
+++
@@ -1,7 +0,0 @@
-from fetch import BrutalistFetch
-from pprint import pprint
-from datetime import date
-
-brutalist_fetch = BrutalistFetch()
-
-print(brutalist_fetch.fetch_sources(topic="https://brutalist.report/topic/tech?"))
