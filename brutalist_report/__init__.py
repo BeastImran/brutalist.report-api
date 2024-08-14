e9a18e34b3afcfcd0835474e62602e53e81fbabc
BeastImran <imsalmanran789@gmail.com>
2024-08-14 12:20:11 +0530

    improve module imports

--- brutalist_report/__init__.py
+++ brutalist_report/__init__.py
@@ -3,5 +3,9 @@
 This package has both sync and async python modules. Use as appropriate.
 
 - `fetch`: Provides a simple interface to interact and scrape different data from Brutalist.report.
-- `async_magic`: Provides an asynchronous interface to interact and scrape different data from Brutalist.report.
+- `async_fetch`: Provides an asynchronous interface to interact and scrape different data from Brutalist.report.
 """
+import fetch
+import async_fetch
+
+__all__ = ["fetch", "async_fetch"]
